b3ba9b7718f40020a5177cb11ddfa53c8872f481
Michael Dunsmuir <mjdunsmuir@gmail.com>
2014-06-24 00:47:37 -0700

    added sphere intersect search in Octree

--- Octree.hs
+++ Octree.hs
@@ -1,6 +1,15 @@
-module Octree where
+module Octree (
+  OctreeLocation,
+  OctreeDatum(..),
+  fromList,
+  pointLookup,
+  sphereLookup,
+  locationSum,
+  locationDiff
+) where
 
 import Data.List
+import Debug.Trace
 
 type OctreeLocation = (Float, Float, Float)
 data OctreeDimensions = OctreeDimensions { center :: OctreeLocation, 
@@ -13,6 +22,93 @@ data Octree a = Octree { contents :: OctreeDatum a, dimensions :: OctreeDimensio
                 deriving Show
 
 data Quadrant = U1 | U2 | U3 | U4 | L1 | L2 | L3 | L4 deriving Show
+
+{-
+quadrant layout (z levels not shown)
+
+y 1 2
+y 3 4
+  x x
+-}
+
+-- creation functions
+
+fromList :: [OctreeDatum a] -> Octree a
+fromList lst = fromList' dims lst
+  where 
+    dims = dimsFromLocs $ map location lst
+
+    fromList' :: OctreeDimensions -> [OctreeDatum a] -> Octree a
+    fromList' _ [] = Empty
+    fromList' dims (x:[]) = Octree x dims
+                            Empty Empty Empty Empty Empty Empty Empty Empty
+    fromList' dims@(OctreeDimensions center _) (x:xs) = 
+      fromFoldResult x dims $ foldr (octantFold center) octantFoldStart xs
+
+    fromFoldResult v dims (u1s, u2s, u3s, u4s, l1s, l2s, l3s, l4s) =
+      let [u1d, u2d, u3d, u4d, l1d, l2d, l3d, l4d] = map (newDims dims) [(-1.0, 1.0, 1.0),
+                                                                         (1.0, 1.0, 1.0),
+                                                                         (-1.0, -1.0, 1.0),
+                                                                         (1.0, -1.0, 1.0),
+                                                                         (-1.0, 1.0, -1.0),
+                                                                         (1.0, 1.0, -1.0),
+                                                                         (-1.0, -1.0, -1.0),
+                                                                         (1.0, -1.0, -1.0)]
+      in  Octree v dims 
+          (fromList' u1d u1s)
+          (fromList' u2d u2s)
+          (fromList' u3d u3s)
+          (fromList' u4d u4s)
+          (fromList' l1d l1s)
+          (fromList' l2d l2s)
+          (fromList' l3d l3s)
+          (fromList' l4d l4s)
+
+-- lookup functions
+
+pointLookup :: OctreeLocation -> Octree a -> [a]
+pointLookup _ Empty = []
+pointLookup loc oct = if location (contents oct) == loc
+                        then value (contents oct) : pointLookup loc (nextOct oct)
+                        else pointLookup loc $ nextOct oct
+  where nextOct = accessorForQuadrant $ getQuadrant (center $ dimensions oct) loc
+
+sphereLookup :: OctreeLocation -> Float -> Octree a -> [a]
+sphereLookup _ _ Empty = []
+sphereLookup center' radius (Octree (OctreeDatum value _) dims u1 u2 u3 u4 l1 l2 l3 l4)
+  | sphereIntersects && useThisDatum = value : resultsFromChildren
+  | sphereIntersects                 = resultsFromChildren
+  | otherwise                        = []
+  where
+    useThisDatum = (radius>=) $ locationDistance center' (center dims)
+    resultsFromChildren = 
+      (foldr1 (++) $ map (sphereLookup center' radius) [u1, u2, u3, u4, l1, l2, l3, l4])
+    sphereIntersects :: Bool
+    sphereIntersects = (>0) $ foldr fun radiusSquared [(xCen, xC1, xC2), (yCen, yC1, yC2), (zCen, zC1, zC2)]
+      where
+        radiusSquared = radius**2.0
+        sideLength' = sideLength dims / 2.0
+        diag = (sideLength', sideLength', sideLength')
+        (xC1, yC1, zC1) = locationDiff (center dims) diag
+        (xC2, yC2, zC2) = locationSum (center dims) diag
+        (xCen, yCen, zCen) = center'
+        fun (vCen, vC1, vC2) r2 = r2'
+          where r2' | vCen < vC1 = r2 - (vCen - vC1)**2.0 
+                    | vCen > vC2 = r2 - (vCen - vC2)**2.0 
+                    | otherwise  = r2
+
+-- define some "vector operations"
+
+locationElemOper :: (Float -> Float -> Float) -> OctreeLocation -> OctreeLocation -> OctreeLocation
+locationElemOper oper (x1, y1, z1) (x2, y2, z2) = (x1 `oper` x2, y1 `oper` y2, z1 `oper` z2)
+
+locationSum = locationElemOper (+)
+locationDiff = locationElemOper (-)
+
+locationDistance :: OctreeLocation -> OctreeLocation -> Float
+locationDistance (x1, y1, z1) (x2, y2, z2) = sqrt $ sum $ map (**2.0) [x1 - x2, y1 - y2, y1 - y2]
+
+-- internal functions
 
 dimsFromLocs :: [OctreeLocation] -> OctreeDimensions
 dimsFromLocs locs = OctreeDimensions (xMid, yMid, zMid) (maximum [xSide, ySide, zSide])
@@ -68,46 +164,5 @@ newDims (OctreeDimensions (x, y, z) sideLength) (xInc, yInc, zInc) =
   let absInc = sideLength / 4.0
    in OctreeDimensions (x + xInc * absInc, y + yInc * absInc, z + zInc * absInc) (sideLength / 2.0)
 
-{-
-y 1 2
-y 3 4
-  x x
--}
-
-fromFoldResult v dims (u1s, u2s, u3s, u4s, l1s, l2s, l3s, l4s) =
-  let [u1d, u2d, u3d, u4d, l1d, l2d, l3d, l4d] = map (newDims dims) [(-1.0, 1.0, 1.0),
-                                                                     (1.0, 1.0, 1.0),
-                                                                     (-1.0, -1.0, 1.0),
-                                                                     (1.0, -1.0, 1.0),
-                                                                     (-1.0, 1.0, -1.0),
-                                                                     (1.0, 1.0, -1.0),
-                                                                     (-1.0, -1.0, -1.0),
-                                                                     (1.0, -1.0, -1.0)]
-  in  Octree v dims 
-      (fromList' u1d u1s)
-      (fromList' u2d u2s)
-      (fromList' u3d u3s)
-      (fromList' u4d u4s)
-      (fromList' l1d l1s)
-      (fromList' l2d l2s)
-      (fromList' l3d l3s)
-      (fromList' l4d l4s)
 
 
-fromList' :: OctreeDimensions -> [OctreeDatum a] -> Octree a
-fromList' _ [] = Empty
-fromList' dims (x:[]) = Octree x dims
-                        Empty Empty Empty Empty Empty Empty Empty Empty
-fromList' dims@(OctreeDimensions center _) (x:xs) = 
-  fromFoldResult x dims $ foldr (octantFold center) octantFoldStart xs
-
-fromList :: [OctreeDatum a] -> Octree a
-fromList lst = fromList' dims lst
-  where dims = dimsFromLocs $ map location lst
-
-pointLookup :: OctreeLocation -> Octree a -> [a]
-pointLookup _ Empty = []
-pointLookup loc oct = if location (contents oct) == loc
-                     then value (contents oct) : pointLookup loc (nextOct oct)
-                     else pointLookup loc $ nextOct oct
-  where nextOct = accessorForQuadrant $ getQuadrant (center $ dimensions oct) loc
